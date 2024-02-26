f10715649e50680329f9c4491cd5514ed70e162f
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-02-26 02:54:03 -0300

    build: project

---
+++ src/main/java/code/blurone/uteams/UserTeams.kt
@@ -0,0 +1,13 @@
+package code.blurone.uteams
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class UserTeams : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
