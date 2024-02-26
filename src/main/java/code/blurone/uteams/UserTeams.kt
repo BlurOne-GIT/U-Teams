2b42d23998b0e2ab499811fd9fb4f49229356d1b
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-02-26 05:39:16 -0300

    build: shade CommandAPI

--- src/main/java/code/blurone/uteams/UserTeams.kt
+++ src/main/java/code/blurone/uteams/UserTeams.kt
@@ -1,13 +1,24 @@
 package code.blurone.uteams
 
+import dev.jorel.commandapi.CommandAPI
+import dev.jorel.commandapi.CommandAPIBukkitConfig
 import org.bukkit.plugin.java.JavaPlugin
 
 class UserTeams : JavaPlugin() {
+    override fun onLoad() {
+        CommandAPI.onLoad(CommandAPIBukkitConfig(this)
+            .verboseOutput(true) // DEBUG
+            .silentLogs(false)
+        )
+    }
+
     override fun onEnable() {
         // Plugin startup logic
+        CommandAPI.onEnable()
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        CommandAPI.onDisable()
     }
 }
